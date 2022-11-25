6d9c27f2915d66b58bafe0dee25f596a2a67468b
amits.singh <amits.singh_mount_blr@acko.tech>
2022-11-25 10:26:50 +0530

    added webMVC config

---
+++ src/main/java/com/example/translation/configurations/WebMVCConfig.java
@@ -0,0 +1,27 @@
+package com.example.translation.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Value("${cors.origins:}")
+    private String[] allowedOrigins;
+
+    @Override public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
